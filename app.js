5d12f15b3260ccf15c76ce96cbf147d79ac76548
Darshan Singh <darshan.singhkav@gmail.com>
2023-10-06 16:49:39 +0530

    new code

---
+++ app.js
@@ -0,0 +1,36 @@
+var ListOfActivities = [];
+var input = document.getElementById("input");
+var toDoList = document.getElementById("todolist");
+var btn = document.getElementById("button");
+btn.onclick = click;
+
+function click ( ) {
+    ListOfActivities.push(input.value);
+    console.log(ListOfActivities)
+    input.value="";
+    showList();
+}
+
+function showList( ) {
+    toDoList.innerHTML=" "
+    ListOfActivities.forEach(function (n, i) {
+        toDoList.innerHTML += 
+        "<li>" +
+        n +
+        "<a onclick='edititem(" +
+        i +
+        ")'>Edit</a>" +
+        "<a onclick ='delete(" +
+        i + 
+        ")'>&times |</a></li>" 
+    });
+}
+function deleteItem(i) {
+    ListOfActivities.splice(i , 1);
+    showList();
+}
+function edititem(i) {
+    var newText = prompt ("please Enter your new Value");
+    ListOfActivities.splice (i , 1, newText);
+    showList();
+}
